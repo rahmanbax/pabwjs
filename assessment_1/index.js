d234ef6a80178ea471e96a7463f96e08f51488d9
rahmanbax <basyirahman@gmail.com>
2024-04-05 15:09:38 +0700

    v1.3

--- assessment_1/index.js
+++ assessment_1/index.js
@@ -2,3 +2,9 @@ function jumlahDanSelect() {
   var jumlah = document.getElementById("jumlah").value;
   var select_opt = document.getElementById("select_option").value;
 }
+
+let buku = [
+  { nama: "sherlock", jumlah: 2 },
+  { nama: "lupin", jumlah: 2 },
+  { nama: "aladin", jumlah: 2 },
+];
